03a3d01d4bc1b75c4f14721d23bf5d5c82c2bffb
Rosana Infante <infant9390@aol.com>
2023-02-21 15:02:50 -0500

    This is the last SQL Workshop on FloJo

---
+++ data/Workshop Wk 5 Olympiccs GOAT.sql
@@ -0,0 +1,48 @@
+  ----Workshop Wk 5 Olympics GOAT
+  ----Florence Griffith Joyner
+  
+--1. How many Olympic Games (editions) did they compete in?  
+--Chaya
+
+
+
+--2.How many medals did they win? 
+--Chaya
+
+
+--3.What’s their distribution of Gold/Silver/Bronze medals? 
+--Kosta
+
+
+--4.Did they win the most medals for their country? 
+--Kosta
+
+--5. Did they win in both team and individual sports? 
+--ME
+  ---A. won independent sport in 1984 (200 meters) Silver medal & 
+  ----1988 both independent (for 100 & 200 meters) & won Gold
+  ---- & team in 1988 Summer Olympics 1 gold & 1 silver (100 & 400 metres)
+  --- 
+  
+SELECT *
+FROM Olympic_Athlete_Event_Results oaer 
+WHERE athlete like 'Florence G%'
+ 
+
+  
+
+--6. Did they ever lose? 
+--Ali
+
+
+--7. Did they have a rival?  
+--Ali
+
+--8. Did anything surprise you? 
+
+
+--9.Are they the greatest of all time (the GOAT) in their sport?  
+--Victor
+  
+  
+  
